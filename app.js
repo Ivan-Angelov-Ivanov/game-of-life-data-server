1ab6f2624e8743f21f33839e4436457e5f17ad89
Ivan <ivan98angelov@gmail.com>
2021-03-18 15:36:42 +0200

    added read a directory

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
+const { readdirSync } = require('fs');
 
 var app = express();
+
+// database
+mongoose.connect('mongodb://localhost:27017/gameoflife-server', {
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.log(error))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+readdirSync("./routes").map((route) => {
+  app.use("/api", require("./routes/" + route))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
